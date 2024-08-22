4aa9a4b9e86f033157791d1b4472f25db4a250e8
valentinsenk <valentin.senk@gmx.at>
2024-08-22 16:20:11 +0200

    correct path for output?

--- 04_bash_scripts/01_build_models.sh
+++ 04_bash_scripts/01_build_models.sh
@@ -5,7 +5,7 @@
 #SBATCH --array=1-100%20 #submit job array with a maximum of 20 parallel jobs
 #SBATCH --job-name=build_abq_model # Job Name
 #SBATCH --time=01:00:00
-#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1/%A_%a/slurm-%A_%a--%x.log  # Standard output and error log
+#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
 ##SBATCH --licenses="abaqus_teaching@2501@l4.zserv.tuwien.ac.at":6
 
 ### current master root where you are in should be:
