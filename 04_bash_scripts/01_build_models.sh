f204e8809d7e9445615e4b9eff90a0e7906465c9
valentinsenk <valentin.senk@gmx.at>
2024-08-23 18:55:53 +0200

    bash scripts adapted

--- 04_bash_scripts/01_build_models.sh
+++ 04_bash_scripts/01_build_models.sh
@@ -5,14 +5,16 @@
 #SBATCH --array=1-100%20 #submit job array with a maximum of 20 parallel jobs
 #SBATCH --job-name=build_abq_model # Job Name
 #SBATCH --time=01:00:00
-#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
+##SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
+#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
 ##SBATCH --licenses="abaqus_teaching@2501@l4.zserv.tuwien.ac.at":6
 
 ### current master root where you are in should be:
 # /home/vsenk/Droplet_Tests_FEA/01_neural_network_project/
 
 # Define the root directory where the parameter directories are located
-ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1"
+#ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1"
+ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v1"
 # Get the current parameter directory based on the job array index
 PARAM_DIR=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 # Navigate to the directory
