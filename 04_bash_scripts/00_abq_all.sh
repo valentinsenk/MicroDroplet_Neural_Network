165adcb41c8eeecfcccf808e66c668954a8850c5
valentinsenk_CLUSTER <valentin.senk@gmx.at>
2024-09-19 23:46:38 +0200

    new geom samples added

--- 04_bash_scripts/00_abq_all.sh
+++ 04_bash_scripts/00_abq_all.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1  # Run all processes on a single node
-##SBATCH --nodelist=node04.imws.tuwien.ac.at #use the new node
+#SBATCH --nodelist=node04.imws.tuwien.ac.at #use the new node
 #SBATCH --ntasks=4  # Use 4 threads (=2 cpu core)
-##SBATCH --array=1-500%25 #submit job array with a maximum of 20 parallel jobs
-##SBATCH --array=55,170,187,204,234,238,242,245,251,252,259,261,262,265-500%25
-#SBATCH --array=395%1
+#SBATCH --array=1-200%20 #submit job array with a maximum of 20 parallel jobs
 #SBATCH --job-name=run_mech_samples # Job Name
 #SBATCH --time=1-00:00:00
-#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/all_param_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
+#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v4/%03a/slurm-%A_%a--%x.log  # Standard output and error log
 ##SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v2/%03a/slurm-%A_%a--%x.log  # Standard output and error log
 #SBATCH --licenses="abaqus_teaching@2501@l4.zserv.tuwien.ac.at":6
 
 # Define the root directory where the parameter directories are located
-ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/all_param_samples/v1"
+ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v4"
 #ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v2"
 # Get the current parameter directory based on the job array index
 PARAM_DIR=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
