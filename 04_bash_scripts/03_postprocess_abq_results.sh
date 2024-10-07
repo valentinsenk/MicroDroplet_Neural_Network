b37003d5e9afce4ff0b0b8a35f8f88859ba25220
valentinsenk_CLUSTER <valentin.senk@gmx.at>
2024-10-07 16:28:15 +0200

    267 again postprocessed because of lic error

--- 04_bash_scripts/03_postprocess_abq_results.sh
+++ 04_bash_scripts/03_postprocess_abq_results.sh
@@ -2,19 +2,19 @@
 #SBATCH --nodes=1  # Run all processes on a single node
 #SBATCH --nodelist=node04.imws.tuwien.ac.at #use the new node
 #SBATCH --ntasks=2  # Use 2 threads (=1 cpu core)
-#SBATCH --array=32-34%20 #submit job array with a maximum of 20 parallel jobs
+#SBATCH --array=267 #submit job array with a maximum of 20 parallel jobs
 #SBATCH --job-name=postprocess_abq_results # Job Name
 #SBATCH --time=01:00:00
-##SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
-#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
+#SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v5/%03a/slurm-%A_%a--%x.log  # Standard output and error log
+##SBATCH --output=/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v1/%03a/slurm-%A_%a--%x.log  # Standard output and error log
 #SBATCH --licenses="abaqus_teaching@2501@l4.zserv.tuwien.ac.at":6
 
 ### current master root where you are in should be:
 # /home/vsenk/Droplet_Tests_FEA/01_neural_network_project/
 
 # Define the root directory where the parameter directories are located
-#ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v1"
-ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v1"
+ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/geometrical_samples/v5"
+#ROOT_DIR="/home/vsenk/Droplet_Tests_FEA/01_neural_network_project/01_data/parameter_files/mechanical_samples/v1"
 # Get the current parameter directory based on the job array index
 PARAM_DIR=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 # Navigate to the directory
