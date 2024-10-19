dceeb0fb670614240b2ce281b3f187c49586217b
valentinsenk <valentin.senk@gmx.at>
2024-10-19 23:48:41 +0200

    selected param sample v2 (fine mesh) semples generated

--- 02_scripts/01_sampling/sampling_v1.jl
+++ 02_scripts/01_sampling/sampling_v1.jl
@@ -7,8 +7,8 @@ json_dir = "01_data/parameter_ranges"
 json_file = "selected_param_sampling_v1.json"
 
 # Sample output
-num_samples = 300
-output_dir = "01_data/parameter_files/selected_param_samples/v1/"
+num_samples = 500
+output_dir = "01_data/parameter_files/selected_param_samples/v2/"
 
 json_file = joinpath(json_dir, json_file)
 
